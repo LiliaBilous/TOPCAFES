7545143908f7b4af3ee2f926e072525ce7392580
LiliaBilous <liliyabilous.email@gmail.com>
2023-03-09 22:54:22 +0200

    changed header style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,28 +4,46 @@ import { RouterLink } from "vue-router";
 <template>
   <header>
     <div class="wrapper-header">
-      <nav class="header__nav-menu">
-        <ul class="nav-menu">
-          <li class="nav-menu-item">
-            <RouterLink to="/">Головна</RouterLink>
-          </li>
-          <li class="nav-menu-item">
-            <RouterLink to="/franyk">Івано-Франківськ</RouterLink>
-          </li>
-          <li class="nav-menu-item">
-            <RouterLink to="/сherkasy">Черкаси</RouterLink>
-          </li>
-          <li class="nav-menu-item">
-            <RouterLink to="/kyiv">Київ</RouterLink>
-          </li>
-          <li class="nav-menu-item">
-            <RouterLink to="/lviv">Львів</RouterLink>
-          </li>
-          <li class="nav-menu-item">
-            <RouterLink to="/odesa">Одеса</RouterLink>
-          </li>
-        </ul>
-      </nav>
+      <a class="header__logo">Top cafes</a>
+      <div class="header__text-wrapper">
+        <RouterLink class="header__item" to="/">Головна</RouterLink>
+        <a href="#" class="header__item cities">Міста</a>
+        <a href="#" class="header__item cafes">Кав'ярні</a>
+        <a href="#" class="header__item burger-menu">
+          <input
+            type="checkbox"
+            id="burger-menu__checkbox"
+            class="burger-menu__input"
+          />
+          <label for="burger-menu__checkbox" class="burger-menu__label">
+            <i class="material-icons">&#xe5d2;</i>
+          </label>
+          <nav class="burger-menu-wrapper">
+            <ul class="header__nav-menu">
+              <li class="nav-menu-item">
+                <RouterLink to="/franyk">Івано-Франківськ</RouterLink>
+              </li>
+              <li class="nav-menu-item">
+                <RouterLink to="/сherkasy">Черкаси</RouterLink>
+              </li>
+              <li class="nav-menu-item">
+                <RouterLink to="/kyiv">Київ</RouterLink>
+              </li>
+              <li class="nav-menu-item">
+                <RouterLink to="/lviv">Львів</RouterLink>
+              </li>
+              <li class="nav-menu-item">
+                <RouterLink to="/odesa">Одеса</RouterLink>
+              </li>
+            </ul>
+          </nav>
+        </a>
+        <!--  -->
+        <a href="#" class="header__item dark-theme">
+          <i class="material-icons">&#xe430;</i>
+        </a>
+        <a href="#" class="header__item language">EN</a>
+      </div>
     </div>
   </header>
 </template>
