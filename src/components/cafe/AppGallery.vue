b53a7bf70f8563da904e426680760ae04a14c67f
LiliaBilous <liliyabilous.email@gmail.com>
2023-03-12 14:59:23 +0200

    delete lightgallery

--- src/components/cafe/AppGallery.vue
+++ src/components/cafe/AppGallery.vue
@@ -1,27 +1,16 @@
 <template>
-  <lightgallery :settings="{ speed: 500, plugins: plugins }">
-    <ul class="gallery__holder">
-      <GalleryImg v-for="image in gallery" :key="image" :img="image" />
-    </ul>
-  </lightgallery>
+  <ul class="gallery__holder">
+    <GalleryImg v-for="image in gallery" :key="image" :img="image" />
+  </ul>
 </template>
 <script>
-import Lightgallery from "lightgallery/vue";
-import lgThumbnail from "lightgallery/plugins/thumbnail";
-import lgZoom from "lightgallery/plugins/zoom";
-import "lightgallery/css/lightgallery.css";
+
 import GalleryImg from "../cafe/GalleryImg.vue";
 
 export default {
-  components: { Lightgallery, GalleryImg },
+  components: { GalleryImg },
   props: { gallery: Array },
-  data() {
-    return { plugins: [lgThumbnail, lgZoom] };
-  },
 };
 </script>
 <style lang="css" scoped>
-@import "lightgallery/css/lightgallery.css";
-@import "lightgallery/css/lg-thumbnail.css";
-@import "lightgallery/css/lg-zoom.css";
 </style>
