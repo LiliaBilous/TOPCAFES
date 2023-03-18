59a4cea83df8eb2d079a088536d562d4c5921570
LiliaBilous <liliyabilous.email@gmail.com>
2023-03-18 22:16:58 +0200

    work with CitiesList Page

--- src/views/CitiesList.vue
+++ src/views/CitiesList.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <div v-for="city in cities" :key="city.id">
-      <p>{{ city.name }}</p>
-      <p v-html="city.bannerText" />
-      <p>{{ city.bannerTitle }}</p>
-      <p>-----</p>
+    <div v-for="city in cities" :key="city.id" :class="city.bannerImgClass">
+      <div>
+        <div class="city__img-holder">
+          <h2 class="city__header">{{ city.bannerTitle }}</h2>
+          <p class="city__text-wrap" v-html="city.bannerText" />
+          <div class="button-wrapper">
+            <RouterLink to="" class="button">Перейти до огляду</RouterLink>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +18,12 @@ import cities from "../content/cities.json";
 export default {
   data() {
     return { cities };
+  },
+  computed: {
+    city() {
+      //return cities[0];
+      return this.cities.find((city) => city.name === this.$route.name);
+    },
   },
 };
 </script>
