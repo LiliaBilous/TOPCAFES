72c187a94ca56f5936d36a3193174715443ae576
LiliaBilous <liliyabilous.email@gmail.com>
2023-10-12 12:11:21 +0300

    fixed path to cities pages in CityList

--- src/views/CitiesList.vue
+++ src/views/CitiesList.vue
@@ -6,7 +6,7 @@
           <h2 class="city__header">{{ city.bannerTitle }}</h2>
           <p class="city__text-wrap" v-html="city.bannerText" />
           <div class="button-wrapper">
-            <RouterLink :to="`/cities/${cities.path}`" class="button"
+            <RouterLink :to="city.path" class="button"
               >Перейти до огляду</RouterLink
             >
           </div>
@@ -23,11 +23,8 @@ export default {
   },
   computed: {
     city() {
-      //return cities[0];
       return this.cities.find((city) => city.name === this.$route.name);
     },
   },
 };
 </script>
-
-// v-for="city in cities" :key="city.path"
