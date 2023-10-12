bdcfe158ade773d8c6df776d1d795d31f09f0f87
LiliaBilous <liliyabilous.email@gmail.com>
2023-10-12 17:21:37 +0300

    changed burgerMenu onclick

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,44 +1,42 @@
 <script setup>
 import { RouterLink } from "vue-router";
+import { ref } from "vue";
+// const burgerMenu = document.querySelector(".header__burger");
+// const navMenu = document.querySelector(".header__nav-menu");
+// burgerMenu.addEventListener("click", () => {
+//   navMenu.classList.toggle("active");
+const isBurgerMenuVisible = ref(false);
 </script>
 <template>
   <header>
     <div class="wrapper-header">
       <div class="header__logo">
-        <RouterLink class="nav-menu" to="/">Top cafes</RouterLink>
+        <RouterLink class="header__home" to="/">Top cafes</RouterLink>
       </div>
       <div class="header__text-wrapper">
         <RouterLink to="/cafes" class="header__item cafes">Кав'ярні</RouterLink>
         <RouterLink to="/cities" class="header__item cities">Міста</RouterLink>
-        <a href="#" class="header__item burger-menu">
-          <input type="checkbox" id="burger-menu__checkbox" class="burger-menu__input" />
-          <label for="burger-menu__checkbox" class="burger-menu__label">
-            <i class="material-icons">&#xe5d2;</i>
-          </label>
-          <nav class="burger-menu-wrapper">
-            <ul class="header__nav-menu">
-              <li class="nav-menu-item">
-                <RouterLink to="/franyk">Івано-Франківськ</RouterLink>
-              </li>
-              <li class="nav-menu-item">
-                <RouterLink to="/сherkasy">Черкаси</RouterLink>
-              </li>
-              <li class="nav-menu-item">
-                <RouterLink to="/kyiv">Київ</RouterLink>
-              </li>
-              <li class="nav-menu-item">
-                <RouterLink to="/lviv">Львів</RouterLink>
-              </li>
-              <li class="nav-menu-item">
-                <RouterLink to="/odesa">Одеса</RouterLink>
-              </li>
-            </ul>
-          </nav>
-        </a>
+        <a href="#" class="header__item language">EN</a>
         <a href="#" class="header__item dark-theme">
           <i class="material-icons">&#xe430;</i>
         </a>
-        <a href="#" class="header__item language">EN</a>
+        <a
+          @click="isBurgerMenuVisible = !isBurgerMenuVisible"
+          class="header__item header__burger"
+        >
+          <span class="humburger__line"></span>
+          <span class="humburger__line"></span>
+          <span class="humburger__line"></span>
+        </a>
+        <Transition>
+          <nav v-if="isBurgerMenuVisible" class="header__nav-menu">
+            <RouterLink to="/franyk" class="nav-menu-item">Івано-Франківськ</RouterLink>
+            <RouterLink to="/сherkasy" class="nav-menu-item">Черкаси</RouterLink>
+            <RouterLink to="/kyiv" class="nav-menu-item">Київ</RouterLink>
+            <RouterLink to="/lviv" class="nav-menu-item">Львів</RouterLink>
+            <RouterLink to="/odesa" class="nav-menu-item">Одеса</RouterLink>
+          </nav>
+        </Transition>
       </div>
     </div>
   </header>
