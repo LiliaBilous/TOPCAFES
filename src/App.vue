ee7abca1bb59cde372d19878f39ff14dd5dc04e8
LiliaBilous <liliyabilous.email@gmail.com>
2024-01-03 22:29:36 +0200

    styled toggle-button-label

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,10 @@
 import { RouterView } from "vue-router";
 import AppHeader from "./components/AppHeader.vue";
 import AppFooter from "./components/AppFooter.vue";
-// const toggle = () => {
-//     if(document.body.classList.contains("dark")){
-//       document.body.classList.remove("dark");
-//     }else{
-//       document.body.classList.add("dark");
-//     }
-//   }
 </script>
 
 <template>
-  <div class="container" :class="{'theme-dark': nightMode} ">
-    <AppHeader />
-    <RouterView />
-    <AppFooter />
-  </div>
+  <AppHeader />
+  <RouterView />
+  <AppFooter />
 </template>
--- src/components/DarkMode.vue
+++ src/components/DarkMode.vue
@@ -1,31 +1,29 @@
 <template>
-  <div class="header__item dark-theme">
-    <i class="material-icons wb_sunny" v-if="nigthMode" @click="toggle()"
-      >&#xe430;</i
-    >
-    <i class="material-icons mode_night" v-else @click="nigthMode = !nigthMode"
-      >&#xf036;</i
-    >
+  <div class="header__item toggle-button">
+    <input type="checkbox" class="toggle-button-checkbox" id="chk" />
+    <label class="toggle-button-label" for="chk">
+      <span class="material-symbols-outlined">light_mode</span>
+      <span class="material-symbols-outlined">dark_mode</span>
+      <div class="toggle-button-ball"></div>
+    </label>
   </div>
 </template>
 
+//
 <script>
-export default {
-  data() {
-    return {
-      nigthMode: false,
-    };
-  },
-  watch: {
-    nigthMode: function () {
-      localStorage.setItem("nightMode", JSON.stringify(this.nigthMode));
-    },
-  },
-  created() {
-    this.nightMode = JSON.parse(localStorage.getItem("nightMode"));
-  },
-};
+// export default {
+//   data() {
+//     return {
+//       isDarkMode: false,
+//       chkId: "chk",
+//     };
+//   },
+//   methods: {
+//     toggleDarkMode() {
+//       this.isDarkMode = !this.isDarkMode;
+//     }
+//   }
+// };
+//
 </script>
-<style>
-
-</style>
+<style></style>
