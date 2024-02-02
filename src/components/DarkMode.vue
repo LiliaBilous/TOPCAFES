3378571b37a7f3848087c6a1f6a27e5917a22800
LiliaBilous <liliyabilous.email@gmail.com>
2024-02-02 23:36:04 +0200

    minor changes

--- src/components/DarkMode.vue
+++ src/components/DarkMode.vue
@@ -37,8 +37,8 @@
   align-items: center;
   justify-content: space-between;
   position: relative;
-  border: 1px solid var(--color-main-brown);
-  /* border-radius: 5rem; */
+  /* background: rgba(108, 44, 4, 0.733);
+  box-shadow: 0px 0px 50px 20px #401d14; */
   padding: 0.5rem;
   height: 2rem;
   width: 5rem;
@@ -47,7 +47,7 @@
 
 .toggle-button-label .toggle-button-ball {
   position: absolute;
-  background-color: var(--color-main-brown);
+  background-color: var(--color-background);
   border-radius: 50%;
   top: 0.25rem;
   left: 0.5rem;
@@ -56,7 +56,9 @@
   transform: translateX(0);
   transition: transform 0.3s ease;
 }
-
+.material-symbols-outlined {
+  color: var(--color-main-brown);
+}
 .toggle-button-checkbox:checked + .toggle-button-label .toggle-button-ball {
   transform: translateX(2.5rem);
 }
