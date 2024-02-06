5ccec3e8fef2a041d0bf902d43b143e502ad5535
LiliaBilous <liliyabilous.email@gmail.com>
2024-02-06 21:43:36 +0200

    minor changes

--- src/components/DarkMode.vue
+++ src/components/DarkMode.vue
@@ -56,56 +56,7 @@ body.dark-mode {
 .footer.dark-mode {
   color: var(--color-background);
 }
-.mode-toggle {
-  position: relative;
-  padding: 0;
-  width: 44px;
-  height: 24px;
-  min-width: 36px;
-  min-height: 20px;
-  background-color: var(--color-main-brown);
-  border: 0;
-  outline: 0;
-  overflow: hidden;
-  cursor: pointer;
-  z-index: 2;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  -webkit-touch-callout: none;
-  appearance: none;
-  transition: background-color 0.5s ease;
-}
-.mode-toggle .toggle {
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  margin: auto;
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  border: 3px solid transparent;
-  box-shadow: inset 0 0 0 2px #a5abba;
-  overflow: hidden;
-  transition: transform 0.5s ease;
-}
-.mode-toggle #dark-mode {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  border-radius: 50%;
-}
-.mode-toggle &:before {
-  content: "";
-  position: relative;
-  width: 100%;
-  height: 100%;
-  left: 50%;
-  float: left;
-  background: var(--color-light-brown);
-  transition: border-radius 0.5s ease, width 0.5s ease, height 0.5s ease, left 0.5s ease,
-    transform 0.5s ease;
-}
+
 body.dark-mode .mode-toggle {
   background-color: var(--color-background);
 }
