a6b4e55d6d042245c9c97258aedc2f677557dc26
LiliaBilous <liliyabilous.email@gmail.com>
2024-02-13 23:01:04 +0200

    added google map

--- src/components/cities/CityInfo.vue
+++ src/components/cities/CityInfo.vue
@@ -16,10 +16,12 @@
     <div class="city-info-item" :class="city.cityLogoClass"></div>
     <div class="city-info-item">Населення - {{ city.population }} осіб</div>
   </div>
-  <div class="city-map-wrap"></div>
+  <div class="city-map-wrap" id="map">
+  </div>
 </template>
 <script>
 import cities from "../../content/cities.json";
+
 export default {
   data() {
     return {
@@ -29,6 +31,32 @@ export default {
   computed: {
     city() {
       return this.cities.find((city) => city.name === this.$route.name);
+    },
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 50.4501, lng: 30.5234 }, // Координати Києва
+        zoom: 12, // Масштаб мапи
+      });
+
+      // Додайте маркери кав'ярень
+      const cafes = [
+        { lat: 50.4391, lng: 30.5206, name: "Cafe 1" },
+        { lat: 50.4547, lng: 30.5161, name: "Cafe 2" },
+        // Додайте інші кав'ярні за потреби
+      ];
+
+      cafes.forEach((cafe) => {
+        new google.maps.Marker({
+          position: { lat: cafe.lat, lng: cafe.lng },
+          map,
+          title: cafe.name,
+        });
+      });
     },
   },
 };
