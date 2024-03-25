c8af0a579badbf1a6e88da412adf9fc74dbb54c4
Liliia Bilous <liliyabilous.email@gmail.com>
2024-03-25 13:34:11 +0200

    updated loading and error handling for cities

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,12 +1,24 @@
 <template>
   <main class="main-content">
-    <CityInfo />
-    <GoogleMap />
-    <CafeList />
+    <div v-if="isLoading" class="loader">
+      <div class="cup">
+        <span class="steam"></span>
+        <span class="steam"></span>
+        <span class="steam"></span>
+        <div class="cup-handle"></div>
+      </div>
+    </div>
+    <div v-if="errorMessage" class="error">{{ errorMessage }}</div>
+    <template v-else-if="city">
+      <CityInfo />
+      <GoogleMap />
+      <CafeList />
+    </template>
   </main>
 </template>
 <script>
-import CityInfo from "../components/cities/cityinfo.vue";
+import { useCityStore } from "../stores/city";
+import CityInfo from "../components/cities/CityInfo.vue";
 import GoogleMap from "../components/cities/GoogleMap.vue";
 import CafeList from "../components/cities/CafeList.vue";
 
@@ -15,6 +27,38 @@ export default {
     CityInfo,
     GoogleMap,
     CafeList,
+  },
+  data() {
+    return {
+      cityStore: useCityStore(),
+      errorMessage: "",
+      isLoading: true,
+    };
+  },
+  computed: {
+    cities() {
+      return this.cityStore.getCities;
+    },
+    city() {
+      if (!this.cities) {
+        return null;
+      }
+      return this.cities.find((city) => city.name === this.$route.name);
+    },
+  },
+  methods: {
+    async init() {
+      try {
+        await this.cityStore.fetchCity();
+      } catch (error) {
+        this.errorMessage = error.message;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
+  created() {
+    this.init();
   },
 };
 </script>
