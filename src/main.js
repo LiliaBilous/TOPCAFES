7844882a499766bf7a593ba9b84bffc216583902
LiliaBilous <liliyabilous.email@gmail.com>
2025-07-22 13:12:17 +0300

    refactor: Move Google Maps API key to .env and update CI/CD

--- src/main.js
+++ src/main.js
@@ -1,3 +1,52 @@
+function loadGoogleMapsScript() {
+  const googleMapsApiKey = import.meta.env.VITE_MAP_API_KEY;
+
+  if (!googleMapsApiKey) {
+    console.error("Google Maps API Key is missing.");
+    return;
+  }
+
+  ((g) => {
+    var h,
+      a,
+      k,
+      p = "The Google Maps JavaScript API",
+      c = "google",
+      l = "importLibrary",
+      q = "__ib__",
+      m = document,
+      b = window;
+    b = b[c] || (b[c] = {});
+    var d = b.maps || (b.maps = {}),
+      r = new Set(),
+      e = new URLSearchParams(),
+      u = () =>
+        h ||
+        (h = new Promise((f, n) => {
+          a = m.createElement("script");
+          e.set("libraries", [...r] + "");
+          for (k in g)
+            e.set(
+              k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
+              g[k]
+            );
+          e.set("callback", c + ".maps." + q);
+          a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+          d[q] = f;
+          a.onerror = () => (h = n(Error(p + " could not load.")));
+          a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+          m.head.append(a);
+        }));
+    d[l]
+      ? console.warn(p + " only loads once. Ignoring:", g)
+      : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+  })({
+    key: googleMapsApiKey,
+  });
+}
+
+document.addEventListener("DOMContentLoaded", loadGoogleMapsScript);
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
