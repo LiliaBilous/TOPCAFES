aa91c4b0c2d5e82b11e7beb10762ec67c31edadf
LiliaBilous <liliyabilous.email@gmail.com>
2025-07-22 13:12:58 +0300

    chore: deleted console log

--- src/stores/cafes.js
+++ src/stores/cafes.js
@@ -26,7 +26,6 @@ export const useCafeStore = defineStore({
           } else {
             const response = await import("@/content/cafes.json");
             this.cafes = response.default;
-            console.log(this.cafes)
             resolve("foo");
           }
         }, 3000);
